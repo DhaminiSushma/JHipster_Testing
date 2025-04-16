5bedd69193aa078352c83aa98b50ba5e039d41f2
Pasam Dhamini Sushma Sri <pasamdhaminisushma_sri@epam.com>
2025-04-16 15:26:05 +0530

    Removed App classes - Initial Commit

--- Jhipster-Testing-Practice/src/test/java/com/epam/hooks/APIHook.java
+++ Jhipster-Testing-Practice/src/test/java/com/epam/hooks/APIHook.java
@@ -11,16 +11,19 @@ import static io.restassured.RestAssured.given;
 public class APIHook {
     private Response response;
     private final ConfigReader configReader=ConfigReader.getInstance();
+
     @Before
     public void authenticate(){
         Authenticate authenticate=new Authenticate.AuthenticateBuilder().withUsername("admin").withPassword("admin").withRememberMe(true).build();
         //String url= configReader.getProperty("url");
         response=given().body(authenticate).when().post("http://localhost:9000/api/authenticate");
     }
+
     public String getToken(){
         return response.jsonPath().getString("id_token");
     }
     public String getBaseUri(){return configReader.getProperty("url");}
+
     @After
     public void destroy(){
         response=null;
