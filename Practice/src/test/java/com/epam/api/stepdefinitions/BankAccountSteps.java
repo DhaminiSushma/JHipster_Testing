f369c778d88c6eedb9b1d8840bae904d67024788
Pasam Dhamini Sushma Sri <pasamdhaminisushma_sri@epam.com>
2025-04-17 16:18:29 +0530

    Commiting unversioned files

--- Practice/src/test/java/com/epam/api/stepdefinitions/BankAccountSteps.java
+++ Practice/src/test/java/com/epam/api/stepdefinitions/BankAccountSteps.java
@@ -1,5 +1,6 @@
 package com.epam.api.stepdefinitions;
 
+import com.epam.api.models.BankAccount;
 import com.epam.api.utils.ApiUtils;
 import com.epam.api.utils.AuthenticationUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -14,7 +15,7 @@ public class BankAccountSteps {
     private Response response;
     private static ApiUtils apiUtils;
 
-    @Given("User is at the base URL")
+    @Given("User is at the Base URL")
     public void setBaseUrl() throws JsonProcessingException {
         String accessToken = AuthenticationUtil.getToken("admin", "admin");
         apiUtils = new ApiUtils(accessToken);
@@ -24,6 +25,7 @@ public class BankAccountSteps {
     @When("I send a GET request to endpoint {string}")
     public void iSendAGETRequestToEndpoint(String endpoint) {
         response = apiUtils.get(endpoint);
+
     }
 
     @Then("User checks if they Receive Status Code {int}")
@@ -36,5 +38,28 @@ public class BankAccountSteps {
     public void theResponseContentTypeShouldBe(String contentType) {
         String actualContentType = response.getContentType();
         assertEquals(contentType, actualContentType);
+    }
+
+    @When("User enters valid {string} and {string} at endpoint {string}")
+    public void userEntersValidAndAtEndpoint(String name, String balance, String endpoint) {
+        BankAccount bankAccount = new BankAccount.BankAccountBuilder()
+                .setName(name)
+                .setBalance(Double.parseDouble(balance))
+                .build();
+
+        response = apiUtils.post(endpoint, bankAccount);
+    }
+
+    @When("User enters empty fields at endpoint {string}")
+    public void userEntersInvalidIdNameAndBalanceAtEndpoint(String endpoint) {
+        BankAccount bankAccount = new BankAccount.BankAccountBuilder()
+                .build();
+
+        response = apiUtils.put(endpoint, bankAccount);
+    }
+
+    @Then("User checks if the Status Code is {int}")
+    public void userChecksIfTheStatusCodeIs(int statusCode) {
+        assertEquals(statusCode, response.getStatusCode());
     }
 }
--- Practice/src/test/resources/features/api/BankAccount.feature
+++ Practice/src/test/resources/features/api/BankAccount.feature
@@ -2,9 +2,24 @@
 Feature: Bank Account Endpoint
 
   Background:
-    Given User is at the base URL
+    Given User is at the Base URL
 
   Scenario: Valid Get Bank Account Requests
     When I send a GET request to endpoint "/bank-accounts"
     Then User checks if they Receive Status Code 200
     Then the response content type should be "application/json"
+
+  Scenario Template: Valid Bank Accounts Post
+    When User enters valid "<name>" and "<balance>" at endpoint "/bank-accounts"
+    Then User checks if the Status Code is 201
+    Examples:
+      | name    | balance |
+      | Zhongli | 1000.00 |
+      | Albedo  | 2000.00 |
+      | Klee    | 3000.00 |
+      | Diluc   | 4000.00 |
+      | Fischl  | 5000.00 |
+
+  Scenario: Invalid Bank Accounts Post
+    When User enters empty fields at endpoint "/bank-accounts"
+    Then User checks if the Status Code is 405
